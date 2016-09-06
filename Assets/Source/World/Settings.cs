75f00dce719f5c800ac78eace09cb288c28dcdf4
Lomztein <lomztein2@gmail.com>
2016-09-06 22:29:59 +0200

    PROTOTYPE 0.7 - GUI Experiemtations
    - Created a bunch of early GUI sprites.
    - Created a first-pass squad inventory.
    - Added settings menu, and the ability to change GUI color on the fly.
    - Added a font: "Squares" by Jovanny Lemonad of www.dafont.com.

---
+++ Assets/Source/World/Settings.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class Settings : MonoBehaviour {
+
+    public static Settings cur;
+
+    public bool update;
+
+    [Header ("References")]
+    public Canvas[] canvasses;
+
+    [Header ("Settings")]
+    public Color guiImageColor;
+    public Color guiTextColor;
+
+    // Dev settings aren't supposed to be changed from within the game,
+    // and exists more of a utility tool, so that one doesn't have to change
+    // all thing affected manually.
+    [Header ("Dev Settings")]
+    public Font standardFont;
+
+    void Awake () {
+        cur = this;
+    }
+
+    void Update () {
+        if (update) {
+            UpdateGUIColor ();
+        }
+    }
+
+    void UpdateGUIColor () {
+        foreach (Canvas c in canvasses) {
+            Graphic[] graphics = c.GetComponentsInChildren<Graphic> ();
+            foreach (Graphic g in graphics) {
+                if (g.gameObject.tag == "ColoredGUIElement") {
+                    g.color = guiImageColor;
+                    Text t = g.GetComponent<Text> ();
+                    if (t)
+                        t.color = guiTextColor;
+                }
+            }
+        }
+    }
+}
